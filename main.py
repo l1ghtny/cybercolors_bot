245a2451b73c12d2a4910c04d73463e8168db518
l1ghtny <l1ghtny@users.noreply.github.com>
2023-04-11 08:16:14 +0300

    Release v1.8.2 trying to fix psycopg error(k)

--- main.py
+++ main.py
@@ -624,7 +624,7 @@ async def birthday():
                 print(f'{user.name} проверено в:', json_date)
                 print(f'{user.name} дата по timestamp: {json_date_from_timestamp}')
                 if json_date.date() == bd_date.date() and json_date.hour == bd_date.hour:
-                    conn, cursor = await basevariables.access_db_regular()k
+                    conn, cursor = await basevariables.access_db_regular()
                     query2 = 'SELECT * from "public".congratulations where server_id=%s'
                     values2 = (guild_id,)
                     cursor.execute(query2, values2)
