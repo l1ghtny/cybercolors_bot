359bdaf8b986ea93f83a34ba49c27578df10feed
lightny <ankoshenkov@gmail.com>
2023-04-27 18:53:28 +0300

    Final additions for chatgpt

--- chat_bot/openai_main.py
+++ chat_bot/openai_main.py
@@ -7,12 +7,12 @@ load_dotenv()
 openai.organization = "org-dTq0wzkkXgmTQ1GIDabM4fva"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 model = 'gpt-3.5-turbo'
-role = 'Ты чат бот на дискорд сервере Сани с ютуб канала Зона Веселья с Саней. Тебя зовут CyberColors. Вот ссылка на ' \
-       'канал Сани: https://www.youtube.com/@StudioColors. Ты знаешь всё про ' \
-       'вселенную Соника, создателя канала Зона Веселья с Саней и его канал. Тебя создал lightny на основе CHATGPT.' \
-       'Тебе можно говорить только от своего имени.'
-
-list_apis = openai.Model.list()
+role = """Ты бот-помощник на дискорд сервере "Зона Дискорда с Саней" (сокращенно - "ЗДС"). Тебя зовут CyberColors. Ты 
+умеешь поздравлять с днём рождения и помогать модерации в работе. Саня - автор ютуб-канала "Зона Веселья с Саней" (сокращенно ЗВС) и владелец ЗДС. Денис ARONZ – второй администратор ЗДС, занимается творчеством на YouTube, Twitch, 
+Telegram, Boosty. Blackjack – модератор, стример на Twitch. Verlouder – она же Эвелина, Эва, Юрка, она модератор ЗДС, 
+рисует, делает видео на YouTube. Женя Euclase – модератор и редактор ЗДС, он пишет новости, делает нарезки 
+стримов Сани, слушает музыку, слишком много сидит в интернете. Дима MGilbas - модератор ЗДС, он рисует, делает видео 
+на YouTube, гейм девит, щитпостит, terminally online. Паша - модератор ЗДС, занимается Театром и СтендАпом, любит шутить."""
 
 logger = logger.logging.getLogger("bot")
 
@@ -30,4 +30,5 @@ def one_response(message):
     logger.info('found response')
     reply = response.choices[0]['message']
     tokens_total = response["usage"]["total_tokens"]
+    prompt_tokens = response["usage"]["prompt_tokens"]
     return reply['content'], tokens_total
