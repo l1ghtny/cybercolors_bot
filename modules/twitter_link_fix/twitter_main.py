5d6bbc6ad12152c55fe0c94d0cc467ab1c729dd5
lightny <ankoshenkov@gmail.com>
2023-05-18 14:55:09 +0300

    added more informative mention when replying to yourself

--- modules/twitter_link_fix/twitter_main.py
+++ modules/twitter_link_fix/twitter_main.py
@@ -15,23 +15,43 @@ async def twitter_link_replace(message, from_user, reply, attachment):
         if reply is None:
             await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar, files=attachment)
         else:
-            webhook_message = await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar,
-                               files=attachment, wait=True)
-            reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом', url=webhook_message.jump_url)
-            reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url, icon_url=from_user.avatar)
-            await reply.reply('Тебе ответили \U0001F446', embed=reply_embed)
+            if from_user != reply.author:
+                webhook_message = await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar,
+                                   files=attachment, wait=True)
+                reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом', url=webhook_message.jump_url)
+                reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url, icon_url=from_user.avatar)
+                await reply.reply('Тебе ответили \U0001F446', embed=reply_embed)
+            else:
+                webhook_message = await webhook.send(str(new_message), username=from_user.display_name,
+                                                     avatar_url=from_user.avatar,
+                                                     files=attachment, wait=True)
+                reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом',
+                                            url=webhook_message.jump_url)
+                reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url,
+                                       icon_url=from_user.avatar)
+                await reply.reply('Упоминаю это сообщение \U0001F446', embed=reply_embed, silent=True)
     else:
         if reply is None:
             await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar,
                                files=attachment, thread=thread)
         else:
-            webhook_message = await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar,
-                               files=attachment, thread=thread, wait=True)
-            reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом',
-                                        url=webhook_message.jump_url)
-            reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url,
-                                   icon_url=from_user.avatar)
-            await reply.reply('Тебе ответили \U0001F446', embed=reply_embed)
+            if from_user != reply.author:
+                webhook_message = await webhook.send(str(new_message), username=from_user.display_name, avatar_url=from_user.avatar,
+                                   files=attachment, thread=thread, wait=True)
+                reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом',
+                                            url=webhook_message.jump_url)
+                reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url,
+                                       icon_url=from_user.avatar)
+                await reply.reply('Тебе ответили \U0001F446', embed=reply_embed)
+            else:
+                webhook_message = await webhook.send(str(new_message), username=from_user.display_name,
+                                                     avatar_url=from_user.avatar,
+                                                     files=attachment, thread=thread, wait=True)
+                reply_embed = discord.Embed(colour=discord.Colour.dark_blue(), title='Сообщение с ответом',
+                                            url=webhook_message.jump_url)
+                reply_embed.set_author(name=webhook_message.author.display_name, url=webhook_message.jump_url,
+                                       icon_url=from_user.avatar)
+                await reply.reply('Упоминаю это сообщение \U0001F446', embed=reply_embed, silent=True)
     webhooks = await webhook_channel.webhooks()
     for webhook in webhooks:
         await webhook.delete()
