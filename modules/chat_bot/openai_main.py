d6922119de9b0797cd80e578e02c97fa20801d2f
lightny <ankoshenkov@gmail.com>
2024-04-05 16:46:29 +0300

    Updated openAI integration in openai_main.py

--- modules/chat_bot/openai_main.py
+++ modules/chat_bot/openai_main.py
@@ -1,11 +1,14 @@
 import os
 import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 from dotenv import load_dotenv
 from modules.logs_setup import logger
 
 load_dotenv()
-openai.organization = "org-dTq0wzkkXgmTQ1GIDabM4fva"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# TODO: The 'openai.organization' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(organization="org-dTq0wzkkXgmTQ1GIDabM4fva")'
+# openai.organization = "org-dTq0wzkkXgmTQ1GIDabM4fva"
 model = 'gpt-3.5-turbo'
 role = """Ты бот-помощник на дискорд сервере "Зона Дискорда с Саней" (сокращенно - "ЗДС"). Тебя зовут CyberColors. Ты 
 умеешь поздравлять с днём рождения и помогать модерации в работе. Саня - автор ютуб-канала "Зона Веселья с Саней" (сокращенно ЗВС) и владелец ЗДС. Денис ARONZ – второй администратор ЗДС, занимается творчеством на YouTube, Twitch, 
@@ -19,69 +22,63 @@ logger = logger.logging.getLogger("bot")
 
 def one_response(message):
     try:
-        response = openai.ChatCompletion.create(
-            model=model,
+        response = client.chat.completions.create(model=model,
+        temperature=0.4,
+        max_tokens=1024,
+        messages=[
+            {'role': 'system', 'content': role},
+            {'role': 'user', 'content': message}
+        ])
+        logger.info('success')
+        reply = response.choices[0].message
+        content = reply['content']
+        tokens_total = response.usage.total_tokens
+    except openai.RateLimitError as rate_limited:
+        logger.info('rate limited error')
+        logger.error(rate_limited)
+        try:
+            response = client.chat.completions.create(model=model,
             temperature=0.4,
             max_tokens=1024,
             messages=[
                 {'role': 'system', 'content': role},
                 {'role': 'user', 'content': message}
-            ]
-        )
-        logger.info('success')
-        reply = response.choices[0]['message']
-        content = reply['content']
-        tokens_total = response["usage"]["total_tokens"]
-    except openai.error.RateLimitError as rate_limited:
-        logger.info('rate limited error')
-        logger.error(rate_limited)
-        try:
-            response = openai.ChatCompletion.create(
-                model=model,
-                temperature=0.4,
-                max_tokens=1024,
-                messages=[
-                    {'role': 'system', 'content': role},
-                    {'role': 'user', 'content': message}
-                ]
-            )
+            ])
             logger.info('success')
-            reply = response.choices[0]['message']
+            reply = response.choices[0].message
             content = reply['content']
-            tokens_total = response["usage"]["total_tokens"]
-        except openai.error.RateLimitError as rate_limited:
+            tokens_total = response.usage.total_tokens
+        except openai.RateLimitError as rate_limited:
             logger.info('rate limited error')
             content = None
             tokens_total = 0
             logger.error(rate_limited)
-        except openai.error.APIError as api_error:
+        except openai.APIError as api_error:
             logger.info('bad gateway or similar error')
             logger.error(api_error)
             content = '***Ошибка:*** OpenAI упал с рандомной ошибкой. Хз, что у них не так, попробуй ещё раз'
             tokens_total = 0
-    except openai.error.APIError as api_error:
+    except openai.APIError as api_error:
         logger.info('bad gateway or similar error')
         logger.error(api_error)
         try:
-            response = openai.ChatCompletion.create(
-                model=model,
-                temperature=0.4,
-                max_tokens=1024,
-                messages=[
-                    {'role': 'system', 'content': role},
-                    {'role': 'user', 'content': message}
-                ]
-            )
+            response = client.chat.completions.create(model=model,
+            temperature=0.4,
+            max_tokens=1024,
+            messages=[
+                {'role': 'system', 'content': role},
+                {'role': 'user', 'content': message}
+            ])
             logger.info('success')
-            reply = response.choices[0]['message']
+            reply = response.choices[0].message
             content = reply['content']
-            tokens_total = response["usage"]["total_tokens"]
-        except openai.error.RateLimitError as rate_limited:
+            tokens_total = response.usage.total_tokens
+        except openai.RateLimitError as rate_limited:
             logger.info('rate limited error')
             content = None
             tokens_total = 0
             logger.error(rate_limited)
-        except openai.error.APIError as api_error:
+        except openai.APIError as api_error:
             logger.info('bad gateway or similar error')
             logger.error(api_error)
             content = '***Ошибка:*** OpenAI упал с рандомной ошибкой. Хз, что у них не так, попробуй ещё раз'
@@ -94,22 +91,20 @@ def multiple_responses(message_list):
     for i in message_list:
         messages.append(i)
     try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            temperature=0.4,
-            max_tokens=1024,
-            messages=messages
-        )
+        response = client.chat.completions.create(model=model,
+        temperature=0.4,
+        max_tokens=1024,
+        messages=messages)
         logger.info('success')
-        reply = response.choices[0]['message']
+        reply = response.choices[0].message
         content = reply['content']
-        tokens_total = response["usage"]["total_tokens"]
-    except openai.error.RateLimitError as rate_limited:
+        tokens_total = response.usage.total_tokens
+    except openai.RateLimitError as rate_limited:
         logger.info('rate limited error')
         logger.error(rate_limited)
         content = None
         tokens_total = 0
-    except openai.error.APIError as api_error:
+    except openai.APIError as api_error:
         logger.info('bad gateway or similar error')
         logger.error(api_error)
         content = '***Ошибка:*** OpenAI упал с рандомной ошибкой. Хз, что у них не так, попробуй ещё раз'
